409bda74e3ef81f35b9a08eb302263057647a35f
Jaime <jaimegenerali@gmail.com>
2023-01-14 13:01:33 -0300

    autalizações de login e gitIgnore

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,45 +1,80 @@
-const { faker } = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker/locale/pt_BR');
+const perfil = require('../fixtures/perfil.json')
+import 'cypress-map'
 
-const randomName = faker.name.fullName(); 
-const randomEmail = faker.internet.email()
-const fakeName = faker.name.firstName()
+
+
 describe('Testando as funcionalidades da página Minha Conta', () => {
   beforeEach(() => {
     cy.visit('/minha-conta')
     cy.title()
       .should('eq', 'Minha conta – EBAC – Shop')
+    cy.contains('Minha conta')
+    cy.contains('Login')
   })
-context('Menus', () => {
-  it('Averiguando se os menus estão aparecendo', () => {
-    cy.get('.collapse > ul > li').should('have.length', 5)
-    cy.contains('Home')
-      // .should('be.visible')
-    cy.contains('Comprar')
-      .should('be.visible')
-    cy.contains('Blog')
-      .should('be.visible')
-    cy.contains('Categorias')
-      .should('be.visible')
-    cy.contains('Mais vendidos')
-      .should('be.visible')
-  })
-});
-context('Login', () => {
-  it.only('Testando armazenamento de nome na mensagem', () => {
-    cy.get('#username').type('a').then(($texto) => {
-      const text = $texto.text()
-      
-    cy.get('#password').type('ab')
-    
-    cy.get('.woocommerce-form > .button').click()
-    cy.get('.woocommerce-error > li').should(($erro) => {
-      expect($erro.text()).to.eq(`\n\t\t\tErro: a senha informada para o usuário ${text} está incorreta. Perdeu a senha?\t\t`)
+  context('Menus', () => {
+    it('Averiguando se os menus estão aparecendo', () => {
+      cy.exibicaoMenus()
       
     })
-  })
-      
-
   });
-});
-  
+  context('Login', () => {
+    it('Criando um usuário', () => {
+      cy.contains('Sign up').click()
+      cy.contains('Register').should('be.visible')
+      cy.regEmailSenha(faker.internet.email(), faker.internet.password())
+      
+    });
+    
+    it.only('Logando com email  e senha válidos', () => {
+      cy.fixture('perfil').then((dados) => {
+        cy.login()
+        cy.get('.woocommerce-MyAccount-content > p')
+          .should('contain', 'Olá, marli_macedo (não é marli_macedo? Sair)')
+        cy.get('.page-title').should('be.visible')
+
+      })
+    })
+    it('Logando com email e senha inválidos', () => {
+      cy.fixture('perfil').then((dados) => {
+        cy.login(dados.emailInvalido, dados.senha)
+        cy.get('.woocommerce-error > li').should('be.visible')
+         .and('have.text', '\n\t\t\tEndereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.\t\t')
+      })
+    });
+    it('Logando com email válido e senha inválida', () => {
+      cy.fixture('perfil').then((dados) => {
+        cy.login(dados.email, dados.senhaInvalida)
+        cy.get('.woocommerce-error > li').should('be.visible')
+         .and('have.text', '\n\t\t\tErro: a senha fornecida para o e-mail Marli_Macedo@bol.com.br está incorreta. Perdeu a senha?\t\t')
+      })
+    });
+    it('Tentativa de login só digitando o campo Password', () => {
+      cy.fixture('perfil').then((dados) => {
+        cy.contains('a', 'Login').click()
+        cy.get('#password').type(dados.senha, { log: false })
+        cy.get('[name="login"]').click()
+        cy.get('.woocommerce-error > li').should('be.visible')
+         .and('have.text', '\n\t\t\tErro: Nome de usuário é obrigatório.\t\t')
+      })
+    
+    });
+    it('Tentativa de login sem digitar nada', () => {
+      cy.contains('a', 'Login').click()
+      cy.get('[name="login"]').click()
+      cy.get('.woocommerce-error > li').should('be.visible')
+        .and('have.text', '\n\t\t\tErro: Nome de usuário é obrigatório.\t\t')
+    });
+    it('Tentativa de login só digitando o campo username/email', () => {
+      cy.fixture('perfil').then((dados) => {
+        cy.contains('a', 'Login').click()
+        cy.get('#username').type(dados.email)
+        cy.get('[name="login"]').click()
+        cy.get('.woocommerce-error > li').should('be.visible')
+         .and('have.text', '\n\t\t\tErro: o campo da senha está vazio.\t\t')
+      })
+    });
+    
+  })
 })
+
---
+++ cypress/support/loginCommands.js
@@ -0,0 +1,30 @@
+Cypress.Commands.add('login', (email, senha) => {
+    cy.contains('a', 'Login').click()
+        cy.get('#username').type(Cypress.env('email'))
+        cy.get('#password').type(Cypress.env('senha'), { log: false })
+        cy.get('[name="login"]').click()
+
+})
+
+Cypress.Commands.add('regEmailSenha', () => {
+    cy.get('#reg_email').type(Cypress.env('email'))
+      cy.get('#reg_password').type(Cypress.env('senha'))
+      cy.get('[name="register"]').click()
+      cy.get('.woocommerce-MyAccount-content > p')
+        .should('contain', 'Olá, ')
+})
+
+Cypress.Commands.add('exibicaoMenus', () => {
+    cy.get('.collapse > ul > li').should('have.length', 5)
+      //cy.contains não foi conseguiu checar, entao estou usando o xpath
+      cy.xpath("(//a[contains(.,'Home')])[14]").should('be.visible')
+        .and('have.text', 'Home ')
+      cy.xpath("(//a[contains(.,'Comprar')])[2]").should('be.visible')
+        .and('have.text', 'Comprar')
+      cy.xpath("(//a[contains(.,'Blog')])[5]").should('be.visible')
+        .and('have.text', 'Blog ')
+      cy.xpath("(//a[contains(.,'Categorias')])[2]").should('be.visible')
+        .and('have.text', 'Categorias')
+      cy.xpath("(//a[contains(.,'Mais vendidos')])[2]").should('be.visible')
+        .and('have.text', 'Mais vendidos')
+})
